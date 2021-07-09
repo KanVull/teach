6299cc0ebf9edee447487494590864967160a0e7
navimin <minavik99@yandex.ru>
2021-07-09 18:45:48 +0300

    Переделала без bool

--- Code/object.py
+++ Code/object.py
@@ -3,9 +3,9 @@ print("Вы ввели а")
 b = float(input("Введите b:"))
 print("Вы ввели b")
 print("Сумма a+b:", a + b)
-e = bool(a > b)
-if a > b:
+e = a > b
+if e:
     print("Верно")
-elif a < b:
+else:
     print("Неверно")
 
