57c1af3001ae45460d9fd589117b0e725af8a46a
navimin <minavik99@yandex.ru>
2021-07-09 18:39:55 +0300

    Добавила bool

--- Code/object.py
+++ Code/object.py
@@ -3,3 +3,9 @@ print("Вы ввели а")
 b = float(input("Введите b:"))
 print("Вы ввели b")
 print("Сумма a+b:", a + b)
+e = bool(a > b)
+if a > b:
+    print("Верно")
+elif a < b:
+    print("Неверно")
+
